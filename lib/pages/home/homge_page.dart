f9334d70a23b09866251c33c17ba596aef72ec58
Cong Nguyen <congnguyent@nashtechglobal.com>
2023-07-27 20:36:44 +0700

    Implement Appbar for homepage

--- lib/pages/home/homge_page.dart
+++ lib/pages/home/homge_page.dart
@@ -14,9 +14,40 @@ class HomePage extends StatelessWidget {
         return false;
       },
       child: Scaffold(
-        appBar: AppBar(),
+        backgroundColor: Color(0xFFF6EFE8),
+        appBar: AppBar(
+          backgroundColor: Color(0xFFF6EFE8),
+          title: const Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+                Icons.location_on_outlined,
+                color: Color(0xFFAD3F32),
+              ),
+              Text(
+                'Dong Khoi St, District 1',
+                style: TextStyle(
+                    color: Color(0xFF483332),
+                    fontSize: 13,
+                    fontWeight: FontWeight.w500),
+              )
+            ],
+          ),
+          centerTitle: true,
+          actions: [
+            Container(
+              decoration: BoxDecoration(
+                  color: Colors.white, borderRadius: BorderRadius.circular(50)),
+              child: IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.notifications_outlined),
+              ),
+            )
+          ],
+        ),
+        drawer: Drawer(),
         body: const SafeArea(
-          child: Text("Home Hage"),
+          child: Text("Body"),
         ),
       ),
     );
